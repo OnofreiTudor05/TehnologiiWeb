cb56e99307c618e89ed5f1b6eb12346ceacabbb5
CristinBlajut <39240222+CristinBlajut@users.noreply.github.com>
2021-06-10 14:40:46 +0300

    Add files via upload

--- app/core/App.php
+++ app/core/App.php
@@ -11,7 +11,6 @@ class App
 
     public function __construct()
     {
-
         $url = $this->parseazaURL();
         if (file_exists('../app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
